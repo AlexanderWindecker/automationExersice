e5ca5f96d94e06144c4921446c0669882476e4eb
AlexanderWindecker <waa_alex@hotmail.com>
2024-08-23 20:31:19 -0300

    Add config cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,27 @@
 const { defineConfig } = require("cypress");
 
+export default defineConfig({
+  pageLoadTimeout: 20000,
+  viewportWidth: 1920,
+	viewportHeight: 1080,
+	downloadsFolder: 'cypress/downloads',
+	videosFolder: 'cypress/videos',
+	screenshotsFolder: 'cypress/screenshots',
+	screenshotOnRunFailure: true,
+	scrollBehavior: 'center',
+  
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: "cypress.reporter.chrome.json",
+  },
 
-
-
-
-
-
-module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
-    baseUrl:'http://automationexercise.com',
-    specPattern: [ 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}' ],
-		excludeSpecPattern: [ 'cypress/e2e/**/*.example.cy.js' ],
+    baseUrl: "http://automationexercise.com",
+    specPattern: ["cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"],
+    excludeSpecPattern: ["cypress/e2e/**/*.example.cy.js"],
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });

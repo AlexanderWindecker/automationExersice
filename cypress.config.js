14490ad380b9f7ca6ab05b2a6c8befc84f1c3d92
AlexanderWindecker <waa_alex@hotmail.com>
2024-08-23 20:08:22 -0300

    Add Mocha-junit

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,8 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl:'http://automationexercise.com',
+    specPattern: [ 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}' ],
+		excludeSpecPattern: [ 'cypress/e2e/**/*.example.cy.js' ],
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },

f18e36d38978b8736218acf2f4b2195f02a91828
AlexanderWindecker <waa_alex@hotmail.com>
2024-08-26 16:42:37 -0300

    Add artifact

--- cypress.config.js
+++ cypress.config.js
@@ -15,14 +15,12 @@ module.exports = defineConfig({
     configFile: "cypress.reporter.chrome.json",
   },
 
-  // reporter: "cypress-mochawesome-reporter",
   
   e2e: {
     baseUrl: "http://automationexercise.com",
     specPattern: ["cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"],
     excludeSpecPattern: ["cypress/e2e/**/*.example.cy.js"],
     setupNodeEvents(on, config) {
-      // require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
